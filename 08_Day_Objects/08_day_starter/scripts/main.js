5d32278c58e06ca507f5a3a5b365811a6d19988e
Dan Morey <danjmorey@gmail.com>
2021-03-23 17:51:24 -0400

    fumbling through exercises

--- 08_Day_Objects/08_day_starter/scripts/main.js
+++ 08_Day_Objects/08_day_starter/scripts/main.js
@@ -1,2 +1,76 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+//console.log(countries)
+//alert('Open the console and check if the countries has been loaded')
+
+const dog = {
+  name: 'Zooey',
+  legs: 4,
+  color: 'Tri-color',
+  age: 7,
+  bark: function(){
+    console.log('woof woof')
+  }
+}
+dog.breed = 'Great American Mutt'
+
+const users = {
+  Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
+  },
+  Asab: {
+    email: 'asab@asab.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 25,
+    isLoggedIn: false,
+    points: 50
+  },
+  Brook: {
+    email: 'daniel@daniel.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    age: 30,
+    isLoggedIn: true,
+    points: 50
+  },
+  Daniel: {
+    email: 'daniel@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  John: {
+    email: 'john@john.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+    age: 20,
+    isLoggedIn: true,
+    points: 50
+  },
+  Thomas: {
+    email: 'thomas@thomas.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  Paul: {
+    email: 'paul@paul.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  }
+  findMost: function() {
+    return this
+  }
+}
+const keys = Object.keys(users);
+let i = 2;
+//let mostSkills = '';
+//for(let i = 0; i < keys.length; i++){
+  
+//}
+
+console.log(users[i])
